727420ce220e0abda0d9b7ed954108784bd4c199
Oscar <oscarmagallon2002@hotmail.com>
2020-12-11 14:11:56 +0100

    inicio

---
+++ src/juego/Juego.java
@@ -0,0 +1,12 @@
+package juego;
+
+public class Juego {
+	
+	
+
+	public Juego(String nombre) {
+		// TODO Auto-generated constructor stub
+	}
+
+
+}
---
+++ src/juego/Recreativos.java
@@ -0,0 +1,22 @@
+package juego;
+
+public class Recreativos {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String vNombre[]= new String[100];
+		Juego vJuegos[]= new Juego[100];
+		
+		Juego juego1 = new Juego("Ciberpunk");
+		Juego juego2 = new Juego("Los Sims");
+		Juego juego3 = new Juego("Fifa");
+		
+		vJuegos[0]=juego1;
+		vJuegos[1] = juego2;
+		vJuegos[1] = juego3;
+		for (int i = 0; i <vJuegos.length;i++) {
+			System.out.println(vJuegos[i]);
+		}
+	}
+
+}

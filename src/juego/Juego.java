b2c6c2c76339170ef0353c555ef47a4392ad7020
Oscar <oscarmagallon2002@hotmail.com>
2020-12-14 20:32:58 +0100

    estructura

--- src/juego/Juego.java
+++ src/juego/Juego.java
@@ -2,10 +2,54 @@ package juego;
 
 public class Juego {
 	
+	private String nombre;
+	private int registro;
+	private boolean multijugador;
+	
 	
 
 	public Juego(String nombre) {
-		// TODO Auto-generated constructor stub
+		this.nombre=nombre;
+		this.registro=0;
+		this.multijugador=false;
+		
+		
+	}
+
+
+
+	public String getNombre() {
+		return nombre;
+	}
+
+
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+
+
+	public int getRegistro() {
+		return registro;
+	}
+
+
+
+	public void setRegistro(int registro) {
+		this.registro = registro;
+	}
+
+
+
+	public boolean isMultijugador() {
+		return multijugador;
+	}
+
+
+
+	public void setMultijugador(boolean multijugador) {
+		this.multijugador = multijugador;
 	}
 
 
